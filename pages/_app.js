3035e201954c1d5c6a29d4da62f0c89bea3c8127
zxf19961129 <1509609937@qq.com>
2023-11-30 20:12:44 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
-import '@styles/globals.css'
+<!DOCTYPE html>
+<html>
+<head>
+    <title>表白</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>❤️ 表白 ❤️</h1>
+    </header>
 
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+    <div class="container">
+        <div class="left-panel">
+            <h2>我爱你</h2>
+            <p>亲爱的，我想对你说：</p>
+            <form>
+                <input type="text" name="message" placeholder="请输入你想说的话">
+                <br><br>
+                <input type="submit" value="表白">
+            </form>
+        </div>
+        <div class="right-panel">
+            <img src="love.png">
+        </div>
+    </div>
 
-export default Application
+    <footer>
+        <p>© 2023 表白小站</p>
+    </footer>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="script.js"></script>
+</body>
+</html>
