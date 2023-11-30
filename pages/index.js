b766306c43f7e64e9212fd9be08e915a7d108b54
zxf19961129 <1509609937@qq.com>
2023-11-30 20:14:32 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,35 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+<!DOCTYPE html>
+<html>
+<head>
+    <title>表白</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>❤️ 表白 ❤️</h1>
+    </header>
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+    <div class="container">
+        <div class="left-panel">
+            <h2>我爱你</h2>
+            <p>亲爱的，我想对你说：</p>
+            <form>
+                <input type="text" name="message" placeholder="请输入你想说的话">
+                <br><br>
+                <input type="submit" value="表白">
+            </form>
+        </div>
+        <div class="right-panel">
+            <img src="love.png">
+        </div>
     </div>
-  )
-}
+
+    <footer>
+        <p>© 2023 表白小站</p>
+    </footer>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="script.js"></script>
+</body>
+</html>
